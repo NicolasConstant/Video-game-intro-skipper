73fbcdd38002fd5434b17be9299448d4d6079b4b
Nicolas Constant <github@nicolas-constant.com>
2017-03-23 20:46:12 -0400

    Console selection of targeted game and apply enabling/disabling action on intro video

--- Video-game-intro-skipper/VGIS.Console/Program.cs
+++ Video-game-intro-skipper/VGIS.Console/Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using VGIS.Domain.BusinessRules;
 using VGIS.Domain.Domain;
+using VGIS.Domain.Enums;
 using VGIS.Domain.Repositories;
 
 namespace VGIS.Console
@@ -22,32 +23,59 @@ namespace VGIS.Console
             var detectAllGamesStatus = new DetectAllGamesStatus(gameSettingsRepo, installationDirRepo);
 
             // Load all games
-            var allGames = new List<Tuple<GameSetting, GameDetectionResult>>();
-            foreach (var gameStatus in detectAllGamesStatus.Run())
-            {
-                allGames.Add(gameStatus);
-
-                var introMessage = gameStatus.Item2.Detected ?  "Intro Video Enabled" : "Intro Video Disabled";
-                System.Console.WriteLine($"{allGames.Count} - {gameStatus.Item1.Name} : {introMessage} ");
-            }
-
-            System.Console.WriteLine();
-            System.Console.WriteLine("Please select the game for enabling/disabling the intro video");
+            var allGames = LoadAllGames(detectAllGamesStatus);
 
             for (;;)
             {
+                System.Console.WriteLine();
+                System.Console.WriteLine("Please select the game for enabling/disabling the intro video");
+
                 var inputValue = System.Console.ReadLine();
                 int indexValue;
 
                 if (int.TryParse(inputValue, out indexValue))
                 {
-                    
+                    if(indexValue > allGames.Count || indexValue < 1) continue;
+
+                    var gameToModify = allGames[indexValue - 1];
+                    var introState = gameToModify.Item2.IntroductionState;
+                    if (introState == IntroductionStateEnum.Disabled)
+                    {
+                        var reenableIntro = new ApplyReenableIntroAction(gameToModify.Item1, gameToModify.Item2);
+                        reenableIntro.Run();
+                        allGames = LoadAllGames(detectAllGamesStatus);
+                    }
+                    else if (introState == IntroductionStateEnum.Enabled)
+                    {
+                        var reenableIntro = new ApplyDisableIntroAction(gameToModify.Item1, gameToModify.Item2);
+                        reenableIntro.Run();
+                        allGames = LoadAllGames(detectAllGamesStatus);
+                    }
+                    else if(introState == IntroductionStateEnum.Unknown)
+                    {
+                        var reenableIntro = new ApplyDisableIntroAction(gameToModify.Item1, gameToModify.Item2);
+                        reenableIntro.Run();
+                        allGames = LoadAllGames(detectAllGamesStatus);
+                    }
                 }
                 else if (inputValue?.ToLower() == "q" || inputValue?.ToLower() == "quit")
                 {
                     return;
                 }
             }
+        }
+
+        private static List<Tuple<GameSetting, GameDetectionResult>> LoadAllGames(DetectAllGamesStatus detectAllGamesStatus)
+        {
+            var allGames = new List<Tuple<GameSetting, GameDetectionResult>>();
+            foreach (var gameStatus in detectAllGamesStatus.Run())
+            {
+                allGames.Add(gameStatus);
+
+                var introMessage = gameStatus.Item2.Detected ? "Intro Video Enabled" : "Intro Video Disabled";
+                System.Console.WriteLine($"{allGames.Count} - {gameStatus.Item1.Name} : {introMessage} ");
+            }
+            return allGames;
         }
     }
 }
