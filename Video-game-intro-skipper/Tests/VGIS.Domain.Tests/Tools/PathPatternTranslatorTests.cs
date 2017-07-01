b318df7a112a4a3cb49f1524f30594641274d020
Nicolas Constant <github@nicolas-constant.com>
2017-06-30 20:56:46 -0400

    Added a PathPatternTranslator to simplify pattern analysis

---
+++ Video-game-intro-skipper/Tests/VGIS.Domain.Tests/Tools/PathPatternTranslatorTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using VGIS.Domain.BusinessRules;
+using VGIS.Domain.Consts;
+using VGIS.Domain.Domain;
+using VGIS.Domain.Enums;
+using VGIS.Domain.Tools;
+
+namespace VGIS.Domain.Tests.Tools
+{
+    [TestClass]
+    public class PathPatternTranslatorTests
+    {
+        [TestMethod]
+        public void PatternWoNavigation()
+        {
+            //Set constants
+            const string pattern = @"C:\Temp\videos\Nvidia.bk2";
+            
+            //Set Mocks
+            var directoryBrowser = MockRepository.GenerateMock<IDirectoryBrowser>();
+
+            //Run test
+            var businessRule = new PathPatternTranslator(directoryBrowser);
+            var result = businessRule.GetPathFromPattern(pattern).ToList();
+
+            //Validate 
+            directoryBrowser.VerifyAllExpectations();
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(pattern, result.First());
+        }
+        
+        [TestMethod]
+        public void PatternWtNavigation()
+        {
+            //Set constants
+            const string pattern = @"C:\Temp\videos\*\Nvidia.bk2";
+            
+            //Set Mocks
+            var directoryBrowser = MockRepository.GenerateMock<IDirectoryBrowser>();
+            directoryBrowser.Expect(x => x.GetSubDirectories(Arg<string>.Matches(y => y == @"C:\Temp\videos"))).Return(new[] { @"C:\Temp\videos\sub1", @"C:\Temp\videos\sub2" });
+            
+            //Run test
+            var businessRule = new PathPatternTranslator(directoryBrowser);
+            var result = businessRule.GetPathFromPattern(pattern).ToList();
+
+            //Validate 
+            directoryBrowser.VerifyAllExpectations();
+            Assert.AreEqual(2, result.Count);
+            Assert.IsTrue(result.Contains(@"C:\Temp\videos\sub1\Nvidia.bk2"));
+            Assert.IsTrue(result.Contains(@"C:\Temp\videos\sub2\Nvidia.bk2"));
+        }
+
+        [TestMethod]
+        public void PatternWtComplexNavigation()
+        {
+            //Set constants
+            const string pattern = @"C:\Temp\videos\*\*\dummy\second\*\Nvidia.bk2";
+
+            //Set Mocks
+            var directoryBrowser = MockRepository.GenerateMock<IDirectoryBrowser>();
+            directoryBrowser.Expect(x => x.GetSubDirectories(Arg<string>.Matches(y => y == @"C:\Temp\videos"))).Return(new[] { @"C:\Temp\videos\sub1", @"C:\Temp\videos\sub2" });
+            directoryBrowser.Expect(x => x.GetSubDirectories(Arg<string>.Matches(y => y == @"C:\Temp\videos\sub1"))).Return(new[] { @"C:\Temp\videos\sub1\data1", @"C:\Temp\videos\sub1\data2" });
+            directoryBrowser.Expect(x => x.GetSubDirectories(Arg<string>.Matches(y => y == @"C:\Temp\videos\sub2"))).Return(new[] { @"C:\Temp\videos\sub2\data3", @"C:\Temp\videos\sub2\data4" });
+            directoryBrowser.Expect(x => x.GetSubDirectories(Arg<string>.Matches(y => y == @"C:\Temp\videos\sub1\data1\dummy\second"))).Return(new[] { @"C:\Temp\videos\sub1\data1\dummy\second\third1", @"C:\Temp\videos\sub1\data1\dummy\second\third2" });
+            directoryBrowser.Expect(x => x.GetSubDirectories(Arg<string>.Matches(y => y == @"C:\Temp\videos\sub1\data2\dummy\second"))).Return(new[] { @"C:\Temp\videos\sub1\data2\dummy\second\third3", @"C:\Temp\videos\sub1\data2\dummy\second\third4" });
+            directoryBrowser.Expect(x => x.GetSubDirectories(Arg<string>.Matches(y => y == @"C:\Temp\videos\sub2\data3\dummy\second"))).Return(new[] { @"C:\Temp\videos\sub2\data3\dummy\second\third5", @"C:\Temp\videos\sub2\data3\dummy\second\third6" });
+            directoryBrowser.Expect(x => x.GetSubDirectories(Arg<string>.Matches(y => y == @"C:\Temp\videos\sub2\data4\dummy\second"))).Return(new[] { @"C:\Temp\videos\sub2\data4\dummy\second\third7", @"C:\Temp\videos\sub2\data4\dummy\second\third8" });
+
+            //Run test
+            var businessRule = new PathPatternTranslator(directoryBrowser);
+            var result = businessRule.GetPathFromPattern(pattern).ToList();
+
+            //Validate 
+            directoryBrowser.VerifyAllExpectations();
+            Assert.AreEqual(8, result.Count);
+            Assert.IsTrue(result.Contains(@"C:\Temp\videos\sub1\data1\dummy\second\third1\Nvidia.bk2"));
+            Assert.IsTrue(result.Contains(@"C:\Temp\videos\sub1\data1\dummy\second\third2\Nvidia.bk2"));
+            Assert.IsTrue(result.Contains(@"C:\Temp\videos\sub1\data2\dummy\second\third3\Nvidia.bk2"));
+            Assert.IsTrue(result.Contains(@"C:\Temp\videos\sub1\data2\dummy\second\third4\Nvidia.bk2"));
+            Assert.IsTrue(result.Contains(@"C:\Temp\videos\sub2\data3\dummy\second\third5\Nvidia.bk2"));
+            Assert.IsTrue(result.Contains(@"C:\Temp\videos\sub2\data3\dummy\second\third6\Nvidia.bk2"));
+            Assert.IsTrue(result.Contains(@"C:\Temp\videos\sub2\data4\dummy\second\third7\Nvidia.bk2"));
+            Assert.IsTrue(result.Contains(@"C:\Temp\videos\sub2\data4\dummy\second\third8\Nvidia.bk2"));
+        }
+    }
+}
---
+++ Video-game-intro-skipper/VGIS.Domain/Tools/PathPatternTranslator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using VGIS.Domain.Consts;
+
+namespace VGIS.Domain.Tools
+{
+    public class PathPatternTranslator
+    {
+        private readonly IDirectoryBrowser _directoryBrowser;
+
+        #region Ctor
+        public PathPatternTranslator(IDirectoryBrowser directoryBrowser)
+        {
+            _directoryBrowser = directoryBrowser;
+        }
+        #endregion
+
+        public IEnumerable<string> GetPathFromPattern(string pattern)
+        {
+            var pathSections = pattern.Split('\\');
+
+            //If no navigation needed, return the pattern
+            if (!pathSections.Contains(SpecialChar.AnyDirectoryName.ToString()))
+                return new[] { pattern };
+
+            //Analyse the pattern and navigate when needed
+            var allPaths = new List<string>();
+            for (var i = 1; i < pathSections.Length; i++)
+            {
+                var subPath = pathSections[i];
+                //Init
+                if (i == 1)
+                {
+                    allPaths.Add($@"{pathSections[0]}\{pathSections[1]}");
+                }
+                //Is a subfolder
+                else if (!subPath.Contains(SpecialChar.AnyDirectoryName.ToString()))
+                {
+                    for (var j = 0; j < allPaths.Count; j++)
+                    {
+                        var completePath = allPaths[j];
+                        var completeSubPath = $"{completePath}\\{subPath}";
+                        allPaths[j] = completeSubPath;
+                    }
+                }
+                //Is a navigation order
+                else if (subPath.Trim() == SpecialChar.AnyDirectoryName.ToString().Trim())
+                {
+                    var allPathResults = new List<string>();
+                    for (var j = 0; j < allPaths.Count; j++)
+                    {
+                        var completePath = allPaths[j];
+                        var allSubPaths = _directoryBrowser.GetSubDirectories(completePath);
+                        foreach (var allSubPath in allSubPaths)
+                            allPathResults.Add(allSubPath);
+                    }
+                    allPaths = allPathResults;
+                }
+                //Is unsupported
+                else
+                {
+                    throw new ArgumentException($"{pattern} is using a unsupported pattern");
+                }
+            }
+
+            return allPaths;
+        }
+    }
+}
