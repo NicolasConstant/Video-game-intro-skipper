c73f669397273659e65b3d081e9667b5772bad5a
Nicolas Constant <github@nicolas-constant.com>
2017-06-30 21:14:38 -0400

    Clean up

--- Video-game-intro-skipper/VGIS.Domain/BusinessRules/ApplyDisableIntroAction.cs
+++ Video-game-intro-skipper/VGIS.Domain/BusinessRules/ApplyDisableIntroAction.cs
@@ -38,58 +38,6 @@ namespace VGIS.Domain.BusinessRules
                 var pathsToRename = _pathPatternTranslator.GetPathFromPattern(pattern);
                 foreach (var path in pathsToRename)
                     _fileRenamer.RenameFile(path, $@"{path}{GlobalNamesStruct.RenameSuffix}");
-                
-
-                ////If the path doesn't contain '*' symbole, apply renaming directly
-                //if (!originFileFullPath.Contains(SpecialChar.AnyDirectoryName.ToString()))
-                //{
-                //    var destinationFileFullPath = GetCleanPath($"{_detectionResult.InstallationPath}\\{action.InitialName}{GlobalNamesStruct.RenameSuffix}");
-                //    _fileRenamer.RenameFile(originFileFullPath, destinationFileFullPath);
-                //}
-                ////Apply renaming by navigainge througt all '*' alternatives
-                //else
-                //{
-                //    var pathSections = originFileFullPath.Split('\\');
-                //    var allPaths = new List<string>();
-                //    for (var i = 1; i < pathSections.Length; i++)
-                //    {
-                //        var subPath = pathSections[i];
-                //        //Init
-                //        if (i == 1)
-                //        {
-                //            allPaths.Add($@"{pathSections[0]}\{pathSections[1]}");
-                //        }
-                //        //Is a subfolder
-                //        else if (!subPath.Contains(SpecialChar.AnyDirectoryName.ToString()))
-                //        {
-                //            for (var j = 0; j < allPaths.Count; j++)
-                //            {
-                //                var completePath = allPaths[j];
-                //                var completeSubPath = $"{completePath}\\{subPath}";
-                //                allPaths[j] = completeSubPath;
-                //            }
-                //        }
-                //        //Is a navigation order
-                //        else if (subPath.Trim() == SpecialChar.AnyDirectoryName.ToString().Trim())
-                //        {
-                //            var allPathResults = new List<string>();
-                //            for (var j = 0; j < allPaths.Count; j++)
-                //            {
-                //                var completePath = allPaths[j];
-                //                var allSubPaths = _directoryBrowser.GetSubDirectories(completePath);
-                //                foreach (var allSubPath in allSubPaths)
-                //                    allPathResults.Add(allSubPath);
-                //            }
-                //            allPaths = allPathResults;
-                //        }
-                //        //Is unsupported
-                //        else
-                //        {
-                //            throw new ArgumentException($"{originFileFullPath} is using a unsupported pattern");
-                //        }
-                //    }
-
-
 
                 return true;
             }
