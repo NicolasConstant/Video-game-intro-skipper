8350567d12949074c7242f328e0415c98688fe9e
Nicolas Constant <github@nicolas-constant.com>
2017-09-23 17:23:34 -0400

    game listing in now working

--- Video-game-intro-skipper/VGIS.GUI/MainWindowViewModel.cs
+++ Video-game-intro-skipper/VGIS.GUI/MainWindowViewModel.cs
@@ -5,41 +5,74 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Prism.Mvvm;
 using VGIS.GUI.Annotations;
 
 namespace VGIS.GUI
 {
-    public class MainWindowViewModel : INotifyPropertyChanged
+    public class MainWindowViewModel : BindableBase
     {
-        public ObservableCollection<DetectedGame> DetectedGames; 
-        public string Filter { get; set; } = "filter data";
+        private string _filter = "test";
+        private ObservableCollection<DetectedGame> _detectedGames;
+
+        public ObservableCollection<DetectedGame> DetectedGames
+        {
+            get => _detectedGames;
+            set => SetProperty(ref _detectedGames, value);
+        }
+
+        public string Filter
+        {
+            get => _filter;
+            set => SetProperty(ref _filter, value);
+        }
 
         #region Ctor
         public MainWindowViewModel()
         {
-            DetectedGames = new ObservableCollection<DetectedGame>();
+            var list = new List<DetectedGame>();
             for (var i = 0; i < 15; i++)
             {
-                DetectedGames.Add(new DetectedGame()
+                list.Add(new DetectedGame()
                 {
                     ImageUrl = "http://cdn.edgecast.steamstatic.com/steam/apps/493340/header.jpg?t=1504868428",
                 });
             }
+            DetectedGames = new ObservableCollection<DetectedGame>(list);
+
+
+            Task.Run(() =>
+            {
+                Filter = "go";
+                Thread.Sleep(3000);
+                Filter = "loaded";
+                for (var i = 0; i < 15; i++)
+                {
+                    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                    {
+                        DetectedGames.Add(new DetectedGame()
+                        {
+                            ImageUrl = "http://cdn.edgecast.steamstatic.com/steam/apps/493340/header.jpg?t=1504868428",
+                        });
+                    }));
+                }
+            });
         }
         #endregion
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 
-    public class DetectedGame
+    public class DetectedGame : BindableBase
     {
-        public string ImageUrl { get; set; }
+        private string _imageUrl;
+
+        public string ImageUrl
+        {
+            get => _imageUrl;
+            set => SetProperty(ref _imageUrl, value);
+        }
     }
 }
