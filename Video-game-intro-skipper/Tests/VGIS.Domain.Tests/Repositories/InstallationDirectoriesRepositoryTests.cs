0b2d0873b04458cdbd8f88d6884e09df06e61fd8
Nicolas Constant <github@nicolas-constant.com>
2017-11-26 01:16:22 -0500

    Added test for getting installation defaults directories

---
+++ Video-game-intro-skipper/Tests/VGIS.Domain.Tests/Repositories/InstallationDirectoriesRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using VGIS.Domain.DataAccessLayers;
+using VGIS.Domain.Repositories;
+
+namespace VGIS.Domain.Tests.Repositories
+{
+    [TestClass]
+    public class InstallationDirectoriesRepositoryTests
+    {
+        [TestMethod]
+        public void GetAllInstallationFolders_Default()
+        {
+            const string pathToSettingFile = "myCustomPath";
+            var defaultDirectories = "[\"dir1\", \"dir2\"]";
+
+            #region Stubs and Mocks
+            var dalMock = MockRepository.GenerateMock<IFileSystemDal>();
+            dalMock.Expect(x => x.ReadAllText(Arg<string>.Is.Equal(pathToSettingFile))).Return(defaultDirectories);
+            #endregion
+
+            var repo = new InstallationDirectoriesRepository(pathToSettingFile, dalMock);
+            var installationFolders = repo.GetAllInstallationFolders().ToList();
+            
+            #region Validate 
+            dalMock.VerifyAllExpectations();
+            Assert.AreEqual(2, installationFolders.Count);
+            Assert.IsNotNull(installationFolders.Where(x => x.Name == "dir1"));
+            Assert.IsNotNull(installationFolders.Where(x => x.Name == "dir2"));
+            #endregion
+        }
+
+    }
+}
