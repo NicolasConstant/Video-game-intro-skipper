ad7b158e5c288199b364c9879541123e2b779db4
Nicolas Constant <github@nicolas-constant.com>
2017-12-14 00:14:27 -0500

    Added service to validate the illustrations

---
+++ Video-game-intro-skipper/VGIS.Domain/Services/IllustrationValidationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using VGIS.Domain.Enums;
+
+namespace VGIS.Domain.Services
+{
+    public class IllustrationValidationService
+    {
+
+        #region Ctor
+        public IllustrationValidationService()
+        {
+            
+        }
+        #endregion
+
+        public bool IsIllustrationValid(IllustrationPlatformEnum platformType, string illustrationUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
