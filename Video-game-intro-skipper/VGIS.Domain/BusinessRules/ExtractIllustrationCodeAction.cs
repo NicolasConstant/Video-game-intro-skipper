7e825279911caef46ee7399332b11cf3ab059850
Nicolas Constant <github@nicolas-constant.com>
2017-12-26 20:29:10 +0100

    Added action for the illustration's code extraction

---
+++ Video-game-intro-skipper/VGIS.Domain/BusinessRules/ExtractIllustrationCodeAction.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VGIS.Domain.Enums;
+
+namespace VGIS.Domain.BusinessRules
+{
+    public class ExtractIllustrationCodeAction
+    {
+        private readonly IllustrationPlatformEnum _platformType;
+        private readonly string _illustrationUrl;
+
+        #region Ctor
+        public ExtractIllustrationCodeAction(IllustrationPlatformEnum platformType, string illustrationUrl)
+        {
+            _platformType = platformType;
+            _illustrationUrl = illustrationUrl;
+        }
+        #endregion
+
+        public string Execute()
+        {
+            switch (_platformType)
+            {
+                case IllustrationPlatformEnum.Gog:
+                    return ExtractGog(_illustrationUrl);
+                case IllustrationPlatformEnum.BattleNet:
+                    return ExtractBattleNet(_illustrationUrl);
+                case IllustrationPlatformEnum.Origin:
+                    return ExtractOrigin(_illustrationUrl);
+                case IllustrationPlatformEnum.Steam:
+                    return ExtractSteam(_illustrationUrl);
+                case IllustrationPlatformEnum.Uplay:
+                    return ExtractUplay(_illustrationUrl);
+                default:
+                    throw new NotImplementedException();
+
+            }
+        }
+
+        private string ExtractGog(string illustrationUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string ExtractBattleNet(string illustrationUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string ExtractOrigin(string illustrationUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string ExtractSteam(string illustrationUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string ExtractUplay(string illustrationUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
