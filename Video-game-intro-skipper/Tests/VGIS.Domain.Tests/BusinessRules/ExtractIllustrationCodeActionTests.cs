70b6ce818f48660967cbd1eca6cdbbc208becbcb
Nicolas Constant <github@nicolas-constant.com>
2017-12-26 20:40:46 +0100

    Added tests for Illustration Code Extraction (Steam)

---
+++ Video-game-intro-skipper/Tests/VGIS.Domain.Tests/BusinessRules/ExtractIllustrationCodeActionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VGIS.Domain.BusinessRules;
+using VGIS.Domain.Enums;
+
+namespace VGIS.Domain.Tests.BusinessRules
+{
+    [TestClass]
+    public class ExtractIllustrationCodeActionTests
+    {
+        [TestMethod]
+        public void ExtractSteam_ValidPattern()
+        {
+            const string illustrationUrl = "http://cdn.edgecast.steamstatic.com/steam/apps/110800/header.jpg?t=1482775022";
+            const IllustrationPlatformEnum platformType = IllustrationPlatformEnum.Steam;
+
+            var action = new ExtractIllustrationCodeAction(platformType, illustrationUrl);
+            var code = action.Execute();
+
+            #region Validate
+            Assert.AreEqual("110800", code);
+            #endregion
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void ExtractSteam_UnvalidPattern()
+        {
+            const string illustrationUrl = "http://cdn.edgecast.steamstatic.com/steam/aPPpps/110800/header.jpg?t=1482775022";
+            const IllustrationPlatformEnum platformType = IllustrationPlatformEnum.Steam;
+
+            var action = new ExtractIllustrationCodeAction(platformType, illustrationUrl);
+            action.Execute();
+        }
+    }
+}
