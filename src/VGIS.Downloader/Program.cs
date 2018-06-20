eebcd462169a69e4b4b8f6258c405fbb1235d619
Nicolas Constant <github@nicolas-constant.com>
2018-06-19 20:59:12 -0400

    Added console project + settings loading

---
+++ src/VGIS.Downloader/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using VGIS.Downloader.Settings;
+
+
+namespace VGIS.Downloader
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = GetSettings();
+
+
+            Console.WriteLine(settings.StorageAccountCs);
+            Console.ReadKey();
+        }
+
+        private static StorageValues GetSettings()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.prod.json", optional: true);
+
+            var configuration = builder.Build();
+            var settings = new StorageValues();
+            configuration.GetSection("Storage").Bind(settings);
+            return settings;
+        }
+    }
+}
---
+++ src/VGIS.Downloader/Settings/StorageValues.cs
@@ -0,0 +1,10 @@
+﻿namespace VGIS.Downloader.Settings
+{
+    public class StorageValues
+    {
+        public string StorageAccountCs { get; set; }
+        public string ContainerName { get; set; }
+        public string TableName { get; set; }
+    }
+
+}
